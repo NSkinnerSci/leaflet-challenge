366eb9f3743d63bb7a395e04bd4d5ce0367c458f
Nathan Skinner <nathanskinner@Nathans-MacBook-Pro.local>
2023-01-11 16:38:28 -0700

    start part 1

---
+++ Leaflet-Part-1/logic.js
@@ -0,0 +1,15 @@
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+function fetchJSON(url) {
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      });
+  }
